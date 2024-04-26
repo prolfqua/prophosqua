ae19cc81b68ae60c870e525fa0737c31fa84492e
Grossmann <jg@fgcz.ethz.ch>
2024-04-26 16:35:58 +0200

    copy helpers w 2 runscripts

--- R/prophosqua_copy_helpers.R
+++ R/prophosqua_copy_helpers.R
@@ -6,7 +6,8 @@ copy_DEA_FragPipe_DDA <- function(workdir = getwd(), run_script = FALSE) {
   runscripts <- c("application/_DiffExpQC_Phospho.Rmd",
                   "application/_Grp2Analysis_Phospho.Rmd",
                   "application/_Overview_PhosphoAndIntegration.Rmd"
-    if (run_script) {"application/FP_TMT_phos/FP_DDA.R"}
+    if (run_script) {c("application/FP_TMT_phos/FP_TMT_cleanVersion_DEA_enriched_and_total.R",
+                       "application/FP_TMT_phos/FP_TMT_cleanVersion_integration.R"}
   )
   prolfqua::scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prolfquapp")
 }
