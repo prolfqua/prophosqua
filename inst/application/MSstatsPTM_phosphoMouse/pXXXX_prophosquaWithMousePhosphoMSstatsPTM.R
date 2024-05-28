ec0e396b4e25fed6a7dfbac943797e1166c3a790
Grossmann <jg@fgcz.ethz.ch>
2024-05-28 15:38:46 +0200

    DEA for total and phospho run successfully now

--- inst/application/MSstatsPTM_phosphoMouse/pXXXX_prophosquaWithMousePhosphoMSstatsPTM.R
+++ inst/application/MSstatsPTM_phosphoMouse/pXXXX_prophosquaWithMousePhosphoMSstatsPTM.R
@@ -54,6 +54,7 @@ annotable$raw <- annotable$BiolRep
 annotable <- annotable |> rename(Name = BiolRep)
 write_tsv(annotable, file = "annotation.tsv")
 
+# final annotation table
 annot <- prolfquapp::read_annotation(annotable)
 
 
@@ -67,11 +68,10 @@ psm[["plex"]] <- NULL
 psm[["channel"]] <- NULL
 psm$id <- NULL
 psm$z <- NULL
-head(psm)
 psmX <- psm |> group_by(ProteinName,PeptideSequence,pepSeqNcharge,raw) |> summarize(n = n(), Intensity = sum(Intensity)) |> ungroup()
 psmX$n |> table() ## WTF?
 
-
+# adding things
 psmX$PeptideProphet.Probability <- 1
 psmX$qValue <- 0.001
 
@@ -93,35 +93,35 @@ atable$set_response("Intensity")
 # Preprocess data - aggregate proteins.
 config <- prolfqua::AnalysisConfiguration$new(atable)
 psm2 <- dplyr::inner_join(annot$annot, psmX, multiple = "all")
-colnames(psm2)
 adata <- prolfqua::setup_analysis(psm2, config)
 
+# lfq data object
 lfqdata <- prolfqua::LFQData$new(adata, config)
 lfqdata$hierarchy_counts()
 lfqdata$remove_small_intensities(threshold = 1)
 lfqdata$hierarchy_counts()
 
-
+# protein annotation
 pa <- data.frame(protein_Id = unique(lfqdata$data$protein_Id))
 pa <- tidyr::separate(pa, protein_Id , c(NA, "IDcolumn"), sep = "\\|",remove = FALSE)
 pa$description <- "description needed"
 
-# problem
-#protAnnot <- prolfquapp::ProteinAnnotation$new(lfqdata, pa, cleaned_ids = "IDcolumn")
 protAnnot <- ProteinAnnotation$new(lfqdata, pa, cleaned_ids =  "IDcolumn")
-
 protAnnot$row_annot
 
 lfqdata$config$table$hkeysDepth()
 GRP2$processing_options$aggregate
+# aggregate from psm to protein level here
 lfqdata <- prolfquapp::aggregate_data(lfqdata, agg_method = GRP2$processing_options$aggregate)
 
-#logger::log_info("data aggregated: {GRP2$pop$aggregate}.")
+# what is in the game
 lfqdata$factors()
 lfqdata$to_wide()
+
+# grp object
 grp <- prolfquapp::generate_DEA_reports2(lfqdata, GRP2, protAnnot, Contrasts = annot$contrasts)
 
-#debug(write_DEA_all)
+# write reports
 prolfquapp::write_DEA_all(grp2 = grp, boxplot = FALSE, markdown = "_Grp2Analysis_V2.Rmd")
 
 
@@ -149,6 +149,7 @@ path = "."
 (fN <- paste0(fgczProject,"_", descri, "_", WUID,"_",fracti))
 GRP2_phos <- prolfquapp::make_DEA_config_R6(ZIPDIR = fN,PROJECTID = fgczProject,
                                        ORDERID = OIDfgcz)
+# adjust roll-up
 GRP2_phos$processing_options$aggregate <- "topN"
 
 
@@ -157,7 +158,6 @@ fromTSV_ST <- read_tsv("phosphoST-ProteoSAFe-ADD-MASSIVE-REANALYSIS-e9ceea92-dis
 fromTSV_Y <- read_tsv("phosphoY_ProteoSAFe-ADD-MASSIVE-REANALYSIS-e9ceea92-display_quant_results (1)/pY_ADD-MASSIVE-REANALYSIS-e9ceea92-display_quant_results-main.tsv")
 
 stypsm <- data.frame(rbind(fromTSV_ST, fromTSV_Y))
-#head(stypsm)
 colnames(stypsm) <- c("rowid", "ProteinName", "PeptideSequence", "z", "pepSeqNcharge", "plex", "TechRep", "Run", "channel", "Condition", "raw", "Intensity", "id")
 
 # dropping bs and add missing stuff
@@ -174,39 +174,24 @@ stypsmX <- stypsm |> group_by(ProteinName,PeptideSequence,pepSeqNcharge,raw) |> 
 stypsmX$n |> table() ## some are measured twice therefore intensity aggregated w/ sum
 
 # add missing and modify
-# stypsm$idx <- 1:nrow(stypsm)
 stypsmX$PeptideProphet.Probability <- 1
 stypsmX$qValue <- 0.001
-#
-# stypsm$GroupingVariable <- stypsm$Condition
-# stypsm$CONTROL <- "T"
-# stypsm$CONTROL[stypsm$Condition == "WT_Uninfect"] <- "C"
-# #stypsm$channel <- paste(stypsm$Channel, stypsm$Mixture, sep = "_")
-# stypsm$sampleName <- stypsm$BioReplicate
-# stypsm$ProtNsite <- sapply(strsplit((stypsm$ProteinName), split = "\\|"), function(x)x[2])
-# stypsm$Acc <- sapply(strsplit((stypsm$ProtNsite), split = "_"), function(x)x[1]) # parse clean accession
-# stypsm$desc <- "desc"
-# stypsm$ProtNpepSeq <- paste(stypsm$ProtNsite, stypsm$PeptideSequence, sep="~")
-# stypsm$desc <- "myDescription"
-# stypsm$nrPeps <- 1
-# stypsm$BioReplicate <- NULL
-# stypsm$Channel <- stypsm$sampleName
 
 # working on site and protein
 stypsmX$GeneName <- sapply(strsplit((stypsmX$ProteinName), split = "\\|"), function(x)x[1])
 stypsmX$ProtNsite <- sapply(strsplit((stypsmX$ProteinName), split = "\\|"), function(x)x[2])
-unique(stypsmX$ProtNsite) # can have multiple sites: "Q9QZQ1_S193_S196_Y203"
+#unique(stypsmX$ProtNsite) # can have multiple sites: "Q9QZQ1_S193_S196_Y203"
 stypsmX$Acc <- sapply(strsplit((stypsmX$ProtNsite), split = "_"), function(x)x[1])
 
 # Setup configuration
-atable_phos <- annot$atable
+atable_phos <- annot$atable # take same annot table from total
 atable_phos$ident_Score = "PeptideProphet.Probability"
 atable_phos$ident_qValue = "qValue"
 atable_phos$fileName <- "raw"
-atable_phos$hierarchy[["protein_Id"]] <- c("Acc")
-atable_phos$hierarchy[["peptide_Id"]] <- c("ProtNsite")
+atable_phos$hierarchy[["protein_Id"]] <- c("Acc") # change here if different roll-up id should be taken.. e.g. GeneName
+atable_phos$hierarchy[["site"]] <- c("ProtNsite")
 atable_phos$hierarchy[["precursor"]] <- c("pepSeqNcharge")
-atable_phos$hierarchyDepth <- 2 # no roll-up to protein
+atable_phos$hierarchyDepth <- 2 # no roll-up to protein but to protNsite
 
 atable_phos$set_response("Intensity")
 
@@ -214,7 +199,6 @@ atable_phos$set_response("Intensity")
 # Preprocess data - aggregate proteins.
 config_phos <- prolfqua::AnalysisConfiguration$new(atable_phos)
 phospsm2 <- dplyr::inner_join(annot$annot, stypsmX, multiple = "all")
-colnames(phospsm2)
 adata_phos <- prolfqua::setup_analysis(phospsm2, config_phos)
 
 
@@ -224,31 +208,27 @@ lfqdata_phos$hierarchy_counts()
 lfqdata_phos$remove_small_intensities(threshold = 1)
 lfqdata_phos$hierarchy_counts()
 
-colnames(lfqdata_phos$data)
-head(lfqdata_phos$data)
-
 # here we need more parsing w/ site!
 pa_phos <- data.frame(protein_Id = unique(lfqdata_phos$data$protein_Id))
 pa_phos$description <- "description needed"
 pa_phos$IDcolumn <- pa_phos$protein_Id
-#protAnnot_phos <- ProteinAnnotation$new(lfqdata_phos, pa_phos, ids = "protein_Id")
-# still issue here!
 protAnnot_phos <- prolfquapp::ProteinAnnotation$new(lfqdata_phos, pa_phos, cleaned_ids = "IDcolumn")
 protAnnot_phos$row_annot
 
+# aggregating
 lfqdata_phos <- prolfquapp::aggregate_data(lfqdata_phos, agg_method = GRP2_phos$processing_options$aggregate)
+
+# modelling
 grp_phos <- prolfquapp::generate_DEA_reports2(lfqdata_phos, GRP2_phos, protAnnot_phos, Contrasts = annot$contrasts)
 
-lfqdata_phos$to_wide()
-lfqdata$to_wide()
-
-# all fine?
-grp_phos$RES$lfqData$to_wide()
-grp_phos$RES$contrastsData_signif
-myResPlotter <- grp_phos$RES$contrMerged$get_Plotter()
-myResPlotter$volcano()
+# all fine? some checks
+#grp_phos$RES$lfqData$to_wide()
+#grp_phos$RES$contrastsData_signif
+#myResPlotter <- grp_phos$RES$contrMerged$get_Plotter()
+#myResPlotter$volcano()
 
 logger::log_info("DONE WITH DEA REPORTS")
+
 # result dir
 GRP2_phos$zipdir
 dir.create(GRP2_phos$zipdir)
@@ -256,14 +236,7 @@ dir.create(GRP2_phos$zipdir)
 # need helper functions to properly write reports not on protein but peptide level
 # source("FP_phosphoHelperFunctions_v3_202310.R")
 
-colnames(grp_phos$RES$contrastsData)[4] <- "site"
-
-# witold:
+# writing reports
 prolfquapp::write_DEA_all(grp2 = grp_phos, boxplot = FALSE, markdown = "_Grp2Analysis_Phospho_V2.Rmd")
-
-# # jg
-# for (i in seq_along(grp_phos)) {
-#   #prolfquapp::write_DEA_all(grp[[i]], names(grp)[i], GRP2$zipdir, boxplot = FALSE)
-#   write_phosphoDEA_all(grp2 = grp_phos, name = "myNameOfGrp", GRP2_phos$zipdir, boxplot = FALSE)
-# }
+prolfquapp::write_DEA_all(grp2 = grp_phos, boxplot = FALSE, markdown = "_DiffExpQC_Phospho_V2.Rmd")
 
