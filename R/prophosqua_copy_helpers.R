47ad54e12218a857bc4b6e9f1041cb56f0f2565c
Witold Wolski <wewolski@gmail.com>
2024-07-09 15:31:07 +0200

    Pruning deprecated rmd, and using shorter function names.

--- R/prophosqua_copy_helpers.R
+++ R/prophosqua_copy_helpers.R
@@ -2,18 +2,9 @@
 #' @param workdir directory where to copy file - default is current working directory.
 #' @export
 #'
-copy_phosphoDEA_FragPipe_TMT <- function(workdir = getwd(), run_script = FALSE) {
-  runscripts <- c("application/_DiffExpQC_Phospho.Rmd",
-                  "application/_DiffExpQC_Phospho_V2.Rmd",
-                  "application/_Grp2Analysis_Phospho.Rmd",
-                  "application/_Grp2Analysis_Phospho_V2.Rmd",
-                  "application/_Overview_PhosphoAndIntegration.Rmd",
-                  "application/bibliography.bib",
-
-    if (run_script) {c("application/FP_TMT_phos/FP_TMT_cleanVersion_DEA_enriched_and_total.R",
-                       "application/FP_TMT_phos/FP_TMT_cleanVersion_integration.R")}
-    )
-
+copy_phosphoDEA_FragPipe_TMT <- function(workdir = getwd()) {
+  runscripts <- c("application/_DiffExpQC_Phospho_V2.Rmd",
+                  "application/_Grp2Analysis_Phospho_V2.Rmd")
   prolfqua::scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prophosqua")
 }
 
