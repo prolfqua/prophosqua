8c8744afbe50bdb630d0ddf0e4548f1358e07433
Jonas Grossmann <jonas.grossmann@fgcz.uzh.ch>
2025-02-11 15:07:49 +0100

    Adding new builds

--- R/prophosqua_copy_helpers.R
+++ R/prophosqua_copy_helpers.R
@@ -5,11 +5,16 @@
 copy_phosphoDEA_FragPipe_TMT <- function(workdir = getwd()) {
   runscripts <- c("application/_DiffExpQC_Phospho_V2.Rmd",
                   "application/_Grp2Analysis_Phospho_V2.Rmd",
-                  "application/_Overview_PhosphoAndIntegration.Rmd")
+                  "application/_Overview_PhosphoAndIntegration.Rmd",
+                  "inst/application/bibliography.blib"
+                  )
   prolfqua::scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prophosqua")
 }
 
-
+#' copy Markdown and runscript for Spectronaut Ubi workflow
+#' @param workdir directory where to copy file - default is current working directory.
+#' @export
+#'
 copy_ubiDEA_Spectronaut <- function(workdir = getwd()) {
   runscripts <- c(
     "inst/application/_Overview_Ubi_Integration_WEW.Rmd",
