f3cecdacf4bf8b56a8ea4b8b01333c17b5a41372
Grossmann Jonas <jonas.grossmann@uzh.ch>
2025-03-06 14:01:53 +0100

    change bibliography to keep all in

--- R/prophosqua_copy_helpers.R
+++ R/prophosqua_copy_helpers.R
@@ -6,7 +6,7 @@ copy_phosphoDEA_FragPipe_TMT <- function(workdir = getwd()) {
   runscripts <- c("application/_DiffExpQC_Phospho_V2.Rmd",
                   "application/_Grp2Analysis_Phospho_V2.Rmd",
                   "application/_Overview_PhosphoAndIntegration.Rmd",
-                  "inst/application/bibliography.blib"
+                  "inst/application/bibliography2025.blib"
                   )
   prolfqua::scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prophosqua")
 }
@@ -20,7 +20,20 @@ copy_ubiDEA_Spectronaut <- function(workdir = getwd()) {
     "inst/application/_Overview_Ubi_Integration_WEW.Rmd",
     "inst/application/_Grp2Analysis_Ubi.Rmd",
     "inst/application/_DiffExpQC_Ubi.Rmd",
-    "inst/application/bibliography.blib"
+    "inst/application/bibliography2025.blib"
+  )
+  prolfqua::scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prophosqua")
+}
+
+#' copy Markdown and runscript for FragPipe combined_protein.tsv
+#' @param workdir directory where to copy file - default is current working directory.
+#' @export
+#'
+copy_phosphoDEA_Spectronaut <- function(workdir = getwd()) {
+  runscripts <- c("application/_DiffExpQC_Phospho_V2.Rmd",
+                  "application/_Grp2Analysis_Phospho_V2.Rmd",
+                  "application/_Overview_PhosphoAndIntegration_site.Rmd",
+                  "inst/application/bibliography2025.blib"
   )
   prolfqua::scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prophosqua")
 }
