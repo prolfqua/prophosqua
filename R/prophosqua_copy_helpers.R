e34c9222d8603d46bfe8f0e5f011b5b8d0918db4
Witold Wolski <wewolski@gmail.com>
2025-05-26 15:30:47 +0200

    linted R code

--- R/prophosqua_copy_helpers.R
+++ R/prophosqua_copy_helpers.R
@@ -2,11 +2,10 @@
 #' @param workdir directory where to copy file - default is current working directory.
 #' @export
 #'
-copy_phospho_Integration <- function(workdir = getwd()) {
-
-  runscripts <- c("application/_Overview_PhosphoAndIntegration_site.Rmd",
-                  "application/bibliography2025.bib"
-                  )
+copy_phospho_integration <- function(workdir = getwd()) {
+  runscripts <- c(
+    "application/_Overview_PhosphoAndIntegration_site.Rmd",
+    "application/bibliography2025.bib"
+  )
   prolfqua::scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prophosqua")
 }
-
