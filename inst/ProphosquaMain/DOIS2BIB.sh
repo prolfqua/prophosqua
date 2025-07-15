2efcf3ae9d16b6e1f931a84e08ad8c106ff859a5
Witold Wolski <wewolski@gmail.com>
2025-07-15 14:30:22 +0200

    doi2 bib sh

---
+++ inst/ProphosquaMain/DOIS2BIB.sh
@@ -0,0 +1,12 @@
+grep -E -o '10\.[0-9]{4,}/[A-Za-z0-9._\/-]+' Methods_in_MB_PTM2.md | sort -u > dois.txt
+wc dois.txt
+
+python doi_fetch_bib.py
+python doi_to_key.py
+
+python doi_link_to_cite.py Methods_in_MB_PTM2.md Methods_in_MB_PTM2_cite.md
+
+pandoc Methods_in_MB_PTM2_cite.md \
+  --citeproc \
+  -o output.docx
+
